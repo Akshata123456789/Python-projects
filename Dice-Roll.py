0b32dd80d80ae61da2a1fefb394bf6aeb51592fb
Akshata A@ <107637257+Akshata123456789@users.noreply.github.com>
2025-11-12 11:15:12 +0530

    Update Dice-Roll.py

--- Dice-Roll.py
+++ Dice-Roll.py
@@ -1,17 +1,21 @@
 import random
+count = 0
 while True:
-  num = int(input('enter a num how many dice want to roll:'))
-  if num == 0 or num>10:
-     print('invaid input')
+  choice = input('enter y/n:').lower()
+  if choice == 'y':
+       num = int(input('enter how many dice to roll:'))
+       if num <10 or num!=0:
+           for i in range(num):
+                 dice1=random.randint(1,6)
+                 dice2=random.randint(1,6)
+                 print(f'{dice1},{dice2}')
+           count = count+1
+  
+  elif choice == 'n':
+        print('Thanks for playing')
+        print('rolled count is:',count)
+        break
   else:
-        choice = input('enter y/n:').lower()
-        if choice == 'y':
-             for i in range(num):
-                   dice1=random.randint(1,6)
-                   dice2=random.randint(1,6)
-                   print(f'{dice1},{dice2}')
-        elif choice == 'n':
-             print('thanks for playing')
-             break
-        else:
-             print('invalid input')
+        print('invalid input')
+        
+  
